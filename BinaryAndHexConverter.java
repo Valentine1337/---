e6aae169b48873bd17c587fd75b4324628fe0e95
Valentine_cake <88850794+Valentine1337@users.noreply.github.com>
2022-01-14 00:13:44 +0500

    Update BinaryAndHexConverter.java

--- BinaryAndHexConverter.java
+++ BinaryAndHexConverter.java
@@ -6,21 +6,21 @@ import java.util.regex.Pattern;
 Двоично-шестнадцатеричный конвертер
 */
 
-public class BinaryAndHexConverter {
+public class Solution {
     //Константы для того, чтобы сравнивать их элементы между собой и переводить числа из одной системы в другую
     private static final String HEX = "0123456789abcdef";
     private static final String[] BINARY = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001",
             "1010", "1011", "1100", "1101", "1110", "1111"};
 
     public static void main(String[] args) {
-        String binaryNumber = "01";
+        String binaryNumber = "1011101";
         System.out.println("Двоичное число " + binaryNumber + " равно шестнадцатеричному числу " + toHex(binaryNumber));
         String hexNumber = "9d0";
         System.out.println("Шестнадцатеричное число " + hexNumber + " равно двоичному числу " + toBinary(hexNumber));
     }
 
     public static String toHex(String binaryNumber) {
-        String hexNumber = "";
+        StringBuilder hexNumber = new StringBuilder("");
         int indexSubstring = 0;
         if (binaryNumber == null || binaryNumber.equals("") || !binaryNumber.matches("[0-1]+")) {
             return "";
@@ -41,7 +41,7 @@ public class BinaryAndHexConverter {
                 for (int j = 0; j < BINARY.length; j++) {
                     if (firstSubstring.equals(BINARY[j])) {
                         firstSubstring = HEX.charAt(j) + "";
-                        hexNumber = hexNumber + firstSubstring;
+                        hexNumber.append(firstSubstring);
                         break;
                     }
                 }
@@ -51,31 +51,33 @@ public class BinaryAndHexConverter {
                 for (int j = 0; j < BINARY.length; j++) {
                     if (firstSubstring.equals(BINARY[j])) {
                         firstSubstring = HEX.charAt(j) + "";
-                        hexNumber = hexNumber + firstSubstring;
+                        hexNumber.append(firstSubstring);
                         break;
                     }
                 }
             }
             indexSubstring += 4;
         }
-        return hexNumber;
+        String result = new String(hexNumber);
+        return result;
     }
 
     public static String toBinary(String hexNumber) {
         if (hexNumber == null || !hexNumber.matches("[0-9a-f]+")) {
             return "";
         }
-        String binaryNumber = "";
+        StringBuilder binaryNumber = new StringBuilder("");
         for (int i = 0; i < hexNumber.length(); i++) {
             String firstSubstring = hexNumber.substring(i, i + 1);
             for (int j = 0; j < BINARY.length; j++) {
                 String secondSubstring = HEX.substring(j, j + 1);
                 if (firstSubstring.equals(secondSubstring)) {
-                    binaryNumber = binaryNumber + BINARY[j];
+                    binaryNumber.append(BINARY[j]);
                     break;
                 }
             }
         }
-        return binaryNumber;
+        String result = new String(binaryNumber);
+        return result;
     }
 }
